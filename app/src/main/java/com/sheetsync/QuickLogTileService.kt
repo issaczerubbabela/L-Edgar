db5c596c7b46b90013e7f6a6627d030dc0c59239
Issac Zerubbabel A <126003111@sastra.ac.in>
2026-04-01 01:39:26 +0530

    feat: Enhance QuickLogTileService to use PendingIntent for activity launch

--- app/src/main/java/com/sheetsync/QuickLogTileService.kt
+++ app/src/main/java/com/sheetsync/QuickLogTileService.kt
@@ -1,16 +1,40 @@
 package com.sheetsync
 
+import android.app.PendingIntent
+import android.os.Build
 import android.content.Intent
 import android.service.quicksettings.TileService
 
 class QuickLogTileService : TileService() {
 
-    @Suppress("DEPRECATION")
     override fun onClick() {
         super.onClick()
+
         val intent = Intent(this, QuickLogActivity::class.java).apply {
+            action = Intent.ACTION_VIEW
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         }
-        startActivityAndCollapse(intent)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            1001,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val launchAction = {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                startActivityAndCollapse(pendingIntent)
+            } else {
+                @Suppress("DEPRECATION")
+                startActivityAndCollapse(intent)
+            }
+        }
+
+        if (isLocked) {
+            unlockAndRun(launchAction)
+        } else {
+            launchAction()
+        }
     }
 }
